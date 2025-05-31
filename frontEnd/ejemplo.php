1d33ed91c214afef5ad9619d241ea8fc9bfa6e76
JatziryG-G <jatzirygonzalez406@gmail.com>
2025-05-31 15:47:25 -0600

    add new frontend

---
+++ frontEnd/ejemplo.php
@@ -0,0 +1,6 @@
+<?php include "./components/header.php"?>
+<?php include "./components/menu.php"?>
+<main role="main" class="main-content">
+    lorem2000
+</main>
+<?php include "./components/footer.php"?>
